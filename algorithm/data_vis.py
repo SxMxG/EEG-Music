e3f9c814e6fafa4da67c7052c59140328941ad1e
Andrew Placencia <sxmxg01@gmail.com>
2026-03-05 10:22:13 -0800

    main vis

--- algorithm/data_vis.py
+++ algorithm/data_vis.py
@@ -1,24 +1,27 @@
 import mne, time
 from pylsl import StreamInfo, StreamOutlet
 import numpy as np
+def main():
+    raw = mne.io.read_raw_edf("./data", preload=True)
+    data = raw.get_data()
+    sfreq = int(raw.info["sfreq"])
+    n_channels = data.shape[0]
 
-raw = mne.io.read_raw_edf("./data", preload=True)
-data = raw.get_data()
-sfreq = int(raw.info["sfreq"])
-n_channels = data.shape[0]
+    info = StreamInfo("FakeEEG", "EEG", n_channels, sfreq, "float32", "fake_eeg_stream")
+    outlet = StreamOutlet(info)
 
-info = StreamInfo("FakeEEG", "EEG", n_channels, sfreq, "float32", "fake_eeg_stream")
-outlet = StreamOutlet(info)
+    chunk_size = 32
+    speed = 10
+    delay = chunk_size / sfreq * speed
 
-chunk_size = 32
-speed = 10
-delay = chunk_size / sfreq * speed
+    idx = 0
+    while idx < data.shape[1]:
+        chunk = data[:, idx:idx+chunk_size].T
+        for sample in chunk:
+            outlet.push_sample(sample.tolist())
+        time.sleep(delay)
+        print(f"Sent chunk starting at: {idx}")
+        idx += chunk_size
 
-idx = 0
-while idx < data.shape[1]:
-    chunk = data[:, idx:idx+chunk_size].T
-    for sample in chunk:
-        outlet.push_sample(sample.tolist())
-    time.sleep(delay)
-    print(f"Sent chunk starting at: {idx}")
-    idx += chunk_size
+if __name__ == "__main__":
+    main()
